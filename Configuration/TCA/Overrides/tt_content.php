7887239472c7c23b01a9099324a683c4e1a25c68
maispace <joel@maispace.de>
2026-05-22 18:30:28 +0200

    [TASK] Update plugin configuration and add new files for pagination and index

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,31 +4,14 @@ declare(strict_types=1);
 
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    [
-        'label' => 'LLL:EXT:mai_team/Resources/Private/Language/locallang_db.xlf:tt_content.CType.mai_team_view',
-        'value' => 'mai_team_view',
-        'icon' => 'EXT:mai_base/Resources/Public/Icons/generic_content.svg',
-        'group' => 'default',
-    ],
-    'CType',
-    'mai_team'
-);
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-$GLOBALS['TCA']['tt_content']['types']['mai_team_view'] = [
-    'showitem' => '
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-            --palette--;;general,
-            header,
-            pi_flexform,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-            --palette--;;hidden,
-            --palette--;;access,
-    ',
-    'columnsOverrides' => [
-        'pi_flexform' => [
-            'label' => 'LLL:EXT:mai_team/Resources/Private/Language/locallang_db.xlf:tt_content.pi_flexform.mai_team_view',
-            'config' => ['ds' => 'FILE:EXT:mai_team/Configuration/FlexForms/TeamPlugin.xml'],
-        ],
-    ],
-];
+ExtensionUtility::registerPlugin(
+    'MaiTeam',
+    'View',
+    'LLL:EXT:mai_team/Resources/Private/Language/locallang_db.xlf:tt_content.CType.mai_team_view',
+    'mai-content',
+    'default',
+    '',
+    'FILE:EXT:mai_team/Configuration/FlexForms/TeamPlugin.xml',
+);
